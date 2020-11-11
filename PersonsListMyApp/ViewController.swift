e519a856bd75771f68156b6b3c89ee8ac2a77fd2
Mikhail <bskfrost3@yandex.ru>
2020-11-11 20:04:35 +0700

    Initial Commit

---
+++ PersonsListMyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonsListMyApp
+//
+//  Created by mac on 11.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
